2bccd2607160e5ba3cc8fe4a34f57077b444e25e
Shannu-s2 <a.shanmukhasrinu@gmail.com>
2025-07-11 19:11:32 +0530

    fix : typo

--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,7 @@ import RTE from "./RTE";
 
 import Login from "./Login";
 import Button from "./Button";
-import PostForm from "./Post-form/PostForm";
+import PostForm from "./Post-form/Postform";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
 import Input from "./Input";
